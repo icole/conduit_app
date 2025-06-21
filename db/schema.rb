5b694d6e5e40371a7d341cc63d3ba4ec1c26cac7
Anthony Hatch <ahatch1490@gmail.com>
2025-06-21 13:36:15 -0700

    removing tools

--- db/schema.rb
+++ db/schema.rb
@@ -59,18 +59,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_17_000001) do
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
-  create_table "tools", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.string "title"
-    t.string "description"
-    t.boolean "available"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.text "location"
-    t.string "checked_out_by"
-    t.date "expected_return_by"
-    t.string "status", default: "Available"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", null: false
