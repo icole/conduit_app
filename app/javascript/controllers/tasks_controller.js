5362b64fb8a90f2dfb58e2b1c99856bc546fbccc
Ian Cole <ian@colecoding.com>
2025-07-10 09:38:31 -0700

    Fix error from query selector

--- app/javascript/controllers/tasks_controller.js
+++ app/javascript/controllers/tasks_controller.js
@@ -2,19 +2,22 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
+    const form = document.getElementById("new-task-form");
     // Hide the form by default unless there are errors
-    if (!document.querySelector("#new-task-form .alert") && document.getElementById("new-task-form")) {
-      document.getElementById("new-task-form").classList.remove("hidden");
+    if (form && !form.querySelector(".alert")) {
+      form.classList.add("hidden");
     }
   }
 
   showForm(event) {
     event.preventDefault();
-    document.getElementById("new-task-form").classList.remove("hidden");
+    const form = document.getElementById("new-task-form");
+    if (form) form.classList.remove("hidden");
   }
 
   hideForm(event) {
     event.preventDefault();
-    document.getElementById("new-task-form").classList.add("hidden");
+    const form = document.getElementById("new-task-form");
+    if (form) form.classList.add("hidden");
   }
 }
