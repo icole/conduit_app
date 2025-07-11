ddfa4b195e2ab17b2c7e4ecc52a9f285e5bead01
Ian Cole <ian@colecoding.com>
2025-07-10 22:35:42 -0700

    Add user management page

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,46 @@
+class UsersController < ApplicationController
+  before_action :authorize_admin!
+  before_action :set_user, only: [ :edit, :update, :destroy ]
+
+  def index
+    @users = User.all.order(created_at: :desc)
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to users_path, notice: "User was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    # Prevent admins from deleting themselves
+    if @user == current_user
+      redirect_to users_path, alert: "You cannot delete your own account."
+      return
+    end
+
+    # Prevent deletion of other admin users
+    if @user.admin?
+      redirect_to users_path, alert: "Admin users cannot be deleted through this interface."
+      return
+    end
+
+    @user.destroy
+    redirect_to users_path, notice: "User was successfully deleted."
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :admin)
+  end
+end
