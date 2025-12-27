f71817cbff7feb8863ad6edb314ae816c9105c0e
Ian Cole <ian@colecoding.com>
2025-12-27 00:38:44 -0800

    Add cursor rules

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
 ActiveRecord::Schema[8.1].define(version: 2025_12_27_075020) do
+=======
+ActiveRecord::Schema[8.1].define(version: 2025_12_20_074826) do
+>>>>>>> a4917e4 (Update yarn.lock and improve cook signup form)
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
