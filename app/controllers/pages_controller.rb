07661a7f8a99dc8ee07330fc755c415f344318c7
Ian Cole <ian@colecoding.com>
2026-01-09 20:58:45 -0800

    Fix privacy page 404 by skipping tenant filters

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class PagesController < ApplicationController
+  skip_before_action :set_tenant_from_domain
   skip_before_action :authenticate_user!
-  skip_before_action :set_current_tenant
+  skip_before_action :verify_user_belongs_to_tenant!
 
   def privacy
   end
