5babe744a06d6a73e638b7c51e175986480b0780
Ian Cole <ian@colecoding.com>
2026-05-18 10:50:41 -0700

    Harden Sentry: reduce trace sampling, filter sensitive data
    - Reduce traces_sample_rate from 1.0 to 0.1
    - Add before_send filter to drop encoding errors (bot noise)
    - Scrub password/token/secret from request data before sending

--- app/middleware/encoding_error_handler.rb
+++ app/middleware/encoding_error_handler.rb
@@ -6,6 +6,6 @@ class EncodingErrorHandler
   def call(env)
     @app.call(env)
   rescue Encoding::CompatibilityError, Encoding::InvalidByteSequenceError, Encoding::UndefinedConversionError
-    [400, { "content-type" => "text/plain" }, ["Bad Request"]]
+    [ 400, { "content-type" => "text/plain" }, [ "Bad Request" ] ]
   end
 end
--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -5,6 +5,29 @@ if Rails.env.production? && ENV["SENTRY_DSN"].present?
   Sentry.init do |config|
     config.breadcrumbs_logger = [ :active_support_logger ]
     config.dsn = ENV["SENTRY_DSN"]
-    config.traces_sample_rate = 1.0
+
+    # Sample 10% of transactions for performance monitoring
+    config.traces_sample_rate = 0.1
+
+    # Filter sensitive data and noisy errors before sending
+    config.before_send = lambda do |event, hint|
+      exception = hint[:exception]
+
+      # Drop encoding errors from malformed bot/scanner requests
+      if exception.is_a?(Encoding::CompatibilityError) ||
+         exception.is_a?(Encoding::InvalidByteSequenceError) ||
+         exception.is_a?(Encoding::UndefinedConversionError)
+        return nil
+      end
+
+      # Scrub sensitive parameters from request data
+      if event.request&.data.is_a?(Hash)
+        %w[password password_confirmation token secret api_key].each do |key|
+          event.request.data[key] = "[FILTERED]" if event.request.data.key?(key)
+        end
+      end
+
+      event
+    end
   end
 end
